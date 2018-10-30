4fe54b7ddde07fcbff775ef0f8316c0353c9083c
Great Dead Cthulhu <dragnilar@gmail.com>
2018-10-29 23:17:05 -0400

    Added silly unit type conversion library

---
+++ TypeConverter/NUnit.SillyTypeConverter/TestClass.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using SillyTypeConverter;
+
+namespace NUnit.SillyTypeConverter
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void NotNullTests()
+        {
+            try
+            {
+                var bogus = new Faker();
+
+                object conversionDummy = bogus.Random.Int();
+                var randomInt = conversionDummy.ConvertTo<int?>();
+
+                conversionDummy = bogus.Random.String();
+                var randomString = conversionDummy.ConvertTo<string>();
+
+                conversionDummy = bogus.Random.Decimal();
+                var randomDecimal = conversionDummy.ConvertTo<decimal?>();
+
+                conversionDummy = bogus.Random.Bool();
+                var randomBool = conversionDummy.ConvertTo<bool?>();
+
+                conversionDummy = bogus.Random.Long();
+                var randomLong = conversionDummy.ConvertTo<long?>();
+
+                conversionDummy = bogus.Random.Guid();
+                var randomGuid = conversionDummy.ConvertTo<Guid?>();
+
+                Assert.True(randomInt != default);
+                Assert.True(randomString != default);
+                Assert.True(randomDecimal != default);
+                Assert.True(randomBool != default);
+                Assert.True(randomLong != default);
+                Assert.True(randomGuid != default);
+                Console.WriteLine("(づ｡◕‿‿◕｡)づ ALL TESTS PASSED! (｡◕‿‿◕｡)");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Assert.True(false, "An exception occurred, the whole stupid test failed. (╯°□°）╯︵ ┻━┻");
+            }
+        }
+    }
+}
---
+++ TypeConverter/SillyTypeConverter/ExtensionMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SillyTypeConverter
+{
+    public static class ExtensionMethods
+    {
+        public static T ConvertTo<T>(this object valueToConvert)
+        {
+            if (ValueIsNullOrEmptyString(valueToConvert))
+            {
+                return default;
+            }
+            var destinationType = Nullable.GetUnderlyingType(typeof(T));
+            if (destinationType == null)
+            {
+                return (T)Convert.ChangeType(valueToConvert, typeof(T));
+
+            }
+            var result = Convert.ChangeType(valueToConvert, destinationType);
+            return (T)result;
+
+        }
+
+        private static bool ValueIsNullOrEmptyString(object value)
+        {
+            switch (value)
+            {
+                case null:
+                case string s when string.IsNullOrWhiteSpace(s):
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
